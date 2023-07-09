1f39e09635c806887a6f2b45523fd2d23eb6a6ca
sseoheo <127750258+sseoheo@users.noreply.github.com>
2023-07-09 20:41:24 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.cc
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -1,24 +1,12 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
-int main()
-{
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
-    
-    int num;
-    int result = 0;
-
-    for (int i = 0; i < 5; i++) {
-        cin >> num;
-        result += num * num;
-    }
-    
-    result = result % 10;
-
-    cout << result;
-
-    return 0;
+int main(void) {
+	int n, sum(0);
+	for (int i = 0; i < 5; i++) {
+		cin >> n;
+		sum += n * n;
+	}
+	cout << sum % 10;
+	return 0;
 }
