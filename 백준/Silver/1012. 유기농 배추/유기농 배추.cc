a68eb7c78281b72068ac15836b6411fbb1d14e52
sseoheo <127750258+sseoheo@users.noreply.github.com>
2023-07-25 18:53:54 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -4,47 +4,45 @@
 using namespace std;
 
 vector<vector<int>> field;
-int M, N;
+int N, M;
 
 void dfs(int x, int y) {
-    if (x < 0 || x >= M || y < 0 || y >= N || field[y][x] == 0) {
-        return;
-    }
+	if (x < 0 || x >= M || y < 0 || y >= N || field[y][x] == 0) {
+		return;
+	}
 
-    field[y][x] = 0;
-    dfs(x - 1, y);
-    dfs(x + 1, y);
-    dfs(x, y - 1);
-    dfs(x, y + 1);
+	field[y][x] = 0;
+	dfs(x - 1, y);
+	dfs(x + 1, y);
+	dfs(x, y - 1);
+	dfs(x, y + 1);
 }
 
 int main() {
-    int T;
-    cin >> T;
+	int T;
+	cin >> T;
+	
+	while (T--) {
+		int K;
+		cin >> M >> N >> K;
+		field = vector<vector<int>>(N, vector<int>(M, 0));
 
-    while (T--) {
-        int K;
-        cin >> M >> N >> K;
-        field = vector<vector<int>>(N, vector<int>(M, 0));
+		for (int i = 0; i < K; i++) {
+			int x, y;
+			cin >> x >> y;
+			field[y][x] = 1;
+		}
 
-        for (int i = 0; i < K; ++i) {
-            int x, y;
-            cin >> x >> y;
-            field[y][x] = 1;
-        }
-
-        int worm_count = 0;
-        for (int i = 0; i < N; ++i) {
-            for (int j = 0; j < M; ++j) {
-                if (field[i][j] == 1) {
-                    dfs(j, i);
-                    worm_count++;
-                }
-            }
-        }
-
-        cout << worm_count << endl;
-    }
-
-    return 0;
+		int worm_count = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (field[i][j] == 1) {
+					dfs(j, i);
+					worm_count++;
+				}
+			}
+		}
+		cout << worm_count << '\n';
+	}
+	return 0;
 }
