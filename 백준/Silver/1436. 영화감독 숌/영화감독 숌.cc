107f4b9596a4792174225dae0fbde78dfa911057
sseoheo <127750258+sseoheo@users.noreply.github.com>
2023-08-04 23:46:29 +0900

    [Silver V] Title: 영화감독 숌, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -1,25 +1,31 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
+#include<iostream>
 using namespace std;
 
+int N, ans, cnt, temp;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+int main()
+{
+	
+	cin >> N;
 
-    int N;
-    cin >> N;
+	ans = 0; // 영화 제목
+	cnt = 0; // 현재 몇번쨰 종말의 수인지
 
-    int num = 666;
-    string tmp;
-    while (N > 0) {
-        tmp = to_string(num);
-        if (tmp.find("666") != -1) { // 666이 문자열에 포함되면 
-            N--;
-        }
-        num++;
-    }
-    cout << tmp << '\n';
+	while (cnt != N)
+	{
+		ans++;
+		temp = ans;
+
+		// 수에 6이 적어도 3개이상 들어가는지 판별
+		while (temp != 0)
+		{
+			if (temp % 1000 == 666) // 종말의 숫자라면
+			{
+				cnt++;
+				break;
+			}
+			else temp /= 10; // 일의 자리수 삭제
+		}
+	}
+	cout << ans;
 }
-
