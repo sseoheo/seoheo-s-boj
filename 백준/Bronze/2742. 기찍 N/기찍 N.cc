af3b80f37b9931c19d04dfec0f845a4b4997e5ec
sseoheo <127750258+sseoheo@users.noreply.github.com>
2023-08-07 18:34:34 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <tuple>
+using namespace std;
+
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    while (n > 0) {
+        cout << n << '\n';
+        n--;
+    }
+
+    return 0;
+
+}
+
+
