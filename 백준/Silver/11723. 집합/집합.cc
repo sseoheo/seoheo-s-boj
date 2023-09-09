8e21214715dc27bc909d16ad0dc03bee0ddcd383
sseoheo <127750258+sseoheo@users.noreply.github.com>
2023-09-09 19:23:56 +0900

    [Silver V] Title: 집합, Time: 804 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -9,7 +9,7 @@ int main() {
 
     int M,x;
     string Ops;
-    int arr[21] = {};
+    int S = 0;
 
     cin >> M;
     while(M--){
@@ -17,35 +17,33 @@ int main() {
 
         if(Ops == "add"){
             cin >> x;
-            if(arr[x] == 0)
-                arr[x] = 1;
+            // or 연산자를 통해 x번째 자리수를 1로 채운다.
+            S |= (1 << x);
         }
         else if(Ops == "remove"){
             cin >> x;
-            if(arr[x] == 1)
-                arr[x] = 0;
+            // ex) 1000 & ~(1000) = 0000
+            S &= ~(1 << x);
         }
         else if(Ops == "check"){
             cin >> x;
-            if(arr[x] == 1)
+            if(S & (1 << x))
                 cout << 1 << '\n';
             else
                 cout << 0 << '\n';
         }
         else if(Ops == "toggle"){
             cin >> x;
-            if(arr[x] == 1)
-                arr[x] = 0;
-            else
-                arr[x] = 1;
+            // S의 x번째 자리가 1이라면 1^1=0이 되어 1->0이 되고
+            // x번째 자리가 0이라면 0^1=1 이 되어 0 -> 1이 된다.
+            S ^= (1 << x);
         }
         else if(Ops == "all"){
-            for(int i = 1; i <= 20; i++)
-                arr[i] = 1;
+            // ex) 10000 - 1 = 1111
+            S = (1 << 21) - 1;
         }
         else if(Ops == "empty"){
-            for(int i = 1; i <= 20; i++)
-                arr[i] = 0;
+            S = 0;
         }
     }
     return 0;
