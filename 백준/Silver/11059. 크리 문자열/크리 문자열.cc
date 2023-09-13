323ddaf3ec1dae5fd9b39d70b2e55d72e680e91b
sseoheo <127750258+sseoheo@users.noreply.github.com>
2023-09-13 14:11:45 +0900

    [Silver V] Title: 크리 문자열, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Silver/11059. 크리 문자열/크리 문자열.cc
+++ 백준/Silver/11059. 크리 문자열/크리 문자열.cc
@@ -1,45 +1,47 @@
 #include <iostream>
+#include <string>
+#include <stack>
+#include <algorithm>
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+#define MAX 1001
 
 using namespace std;
 
-// 길이가 짝수이고, 앞의 절반의 합과 뒤의 절반의 합이 같은 부분 문자열
 string S;
-int temp = 0;
+int Sum[MAX][MAX];
+int Answer = 0;
 
-void check(int start, int end) {
-    int sum_front = 0;
-    int sum_back = 0;
-    // 앞의 절반 합
-    for (int k = start; k <= start + ((end + 1) / 2) - 1; k++) {
-        sum_front += S[k];
+void Input(){
+    cin >> S;
+}
+
+void Settings() {
+    for (int i = 0; i < S.size(); i++) {
+        Sum[i][i] = (S[i] - '0');
+        for (int j = (i + 1); j < S.size(); j++) {
+            Sum[i][j] = Sum[i][j - 1] + (S[j] - '0');
+        }
     }
-    // 뒤의 절반 합
-    for (int k = start+(end + 1) / 2; k <= start + end; k++) {
-        sum_back += S[k];
+    for (int i = 0; i < S.size(); i++) {
+        int IDX = i;
+        for (int j = (i + 1); j < S.size(); j += 2) {
+            if ((Sum[i][j] % 2 == 0) && ((Sum[i][j] / 2) == Sum[i][IDX])) {
+                Answer = max(Answer, j - i + 1);
+            }
+            IDX++;
+        }
     }
+}
 
-    if (sum_front == sum_back) {
-        temp = max(temp, end + 1);
-    }
-
-    return;
-
+void Find_Answer() {
+    cout << Answer << "\n";
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-
-    cin >> S;
-    for (int i = 0; i < S.size(); i++) {
-        for (int j = 1;j+i < S.size(); j += 2) {
-            check(i, j);
-        }
-    }
-
-    cout << temp;
-
+    FASTIO
+    Input();
+    Settings();
+    Find_Answer();
 
     return 0;
 }
