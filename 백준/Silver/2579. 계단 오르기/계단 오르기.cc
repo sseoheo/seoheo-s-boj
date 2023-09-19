e47491d18198c9d69d58621ede2e3098862f41af
sseoheo <127750258+sseoheo@users.noreply.github.com>
2023-09-19 20:51:39 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -16,7 +16,7 @@ void Input() {
 void Settings() {
     DP[1] = Stair[1];
     DP[2] = Stair[1] + Stair[2];
-    DP[3] = max(Stair[1] + Stair[3], Stair[2] + Stair[3]);
+    DP[3] = max(Stair[1], Stair[2]) + Stair[3];
 
     for(int i = 4; i<= N; i++){
         DP[i] = max(DP[i-2] + Stair[i], DP[i-3] + Stair[i-1] + Stair[i]);
