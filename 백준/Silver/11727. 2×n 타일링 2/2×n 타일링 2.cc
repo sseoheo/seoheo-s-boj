6b369506018c9a035e30e223947eb850cf29f190
sseoheo <127750258+sseoheo@users.noreply.github.com>
2023-10-10 18:22:33 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#define FASTIO std::ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+using namespace std;
+
+int dp[1001];
+
+int main() {
+    FASTIO
+
+    int n;
+    cin >> n;
+
+    dp[1] = 1, dp[2] = 3;
+    for(int i = 3; i <= n; i++){
+        dp[i] = (dp[i-1] + dp[i-2] * 2) % 10007;
+
+    }
+
+    cout << dp[n] ;
+
+    return 0;
+}
