98154af4900dd3b682d3369cc2919db5970c33ce
sseoheo <127750258+sseoheo@users.noreply.github.com>
2023-10-17 16:19:57 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#define FASTIO std::ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+using namespace std;
+
+int main() {
+    FASTIO
+
+    int N, M;
+    cin >> N >> M;
+
+    cout << N * M -1;
+
+    return 0;
+}
