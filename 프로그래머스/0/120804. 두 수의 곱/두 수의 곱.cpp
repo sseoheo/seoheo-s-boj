4d78414c7edbabc69dde6bee5569fb30e4d26b33
sseoheo <127750258+sseoheo@users.noreply.github.com>
2024-03-28 20:18:37 +0900

    [level 0] Title: 두 수의 곱, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    
+    int answer = 0;
+    cin >> num1 >> num2;
+    
+    answer = num1 * num2;
+    
+    return answer;
+}
