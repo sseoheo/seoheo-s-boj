2e5805fbb1d78f819c7286c9f9c139448a3bf0ba
sseoheo <127750258+sseoheo@users.noreply.github.com>
2024-04-01 15:30:29 +0900

    [level 0] Title: 정수 찾기, Time: 0.02 ms, Memory: 60.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(num_list: IntArray, n: Int): Int {
+        var answer: Int = 0
+        for(num in num_list){
+            if(num == n){
+                answer = 1;
+                break;
+            }
+            else
+                answer = 0;
+        }
+        return answer
+    }
+}
